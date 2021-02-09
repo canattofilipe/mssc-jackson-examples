cc9c57acf6e94a85a0ba65aa29b51401802739dd
Wilton Filipe Canatto <wilton@ciandt.com>
2021-02-09 16:25:54 -0300

    using @JsonDeserialize to custom serialization

--- src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoTest.java
+++ src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoTest.java
@@ -11,27 +11,26 @@ import java.io.IOException;
 @JsonTest
 class BeerDtoTest extends BaseTest {
 
-    @Autowired
-    ObjectMapper objectMapper;
+  @Autowired ObjectMapper objectMapper;
 
-    @Test
-    void testSerializeDto() throws JsonProcessingException {
+  @Test
+  void testSerializeDto() throws JsonProcessingException {
 
-        BeerDto beerDto = getDto();
+    BeerDto beerDto = getDto();
 
-        String jsonString = objectMapper.writeValueAsString(beerDto);
+    String jsonString = objectMapper.writeValueAsString(beerDto);
 
-        System.out.println(jsonString);
-    }
+    System.out.println(jsonString);
+  }
 
-    @Test
-    void testDeserialize() throws IOException {
+  @Test
+  void testDeserialize() throws IOException {
 
-        String json = "{\"beerId\":\"fdd61f40-1520-4503-b90a-4181cd5481d4\",\"beerName\":\"BeerName\",\"beerStyle\":\"Ale\",\"upc\":123123123,\"price\":12.99,\"createdDate\":\"2021-01-30T11:53:03.028743-03:00\",\"lastUpdatedDate\":\"2021-01-30T11:53:03.028806-03:00\"}\n";
+    String json =
+        "{\"beerName\":\"BeerName\",\"beerStyle\":\"Ale\",\"upc\":123123123,\"price\":\"12.99\",\"createdDate\":\"2021-02-09T16:22:01-0300\",\"lastUpdatedDate\":\"2021-02-09T16:22:01.54942-03:00\",\"myLocalDate\":\"20210209\",\"beerId\":\"1d890b39-44f2-4f16-b3eb-0deac03779c1\"}\n";
 
-        BeerDto beerDto = objectMapper.readValue(json, BeerDto.class);
+    BeerDto beerDto = objectMapper.readValue(json, BeerDto.class);
 
-        System.out.println(beerDto);
-    }
-
+    System.out.println(beerDto);
+  }
 }
